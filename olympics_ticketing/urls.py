422443d627d64e5b888064a58e76fb201a9bcbcb
Renaud <renaud.durand.eu@gmail.com>
2024-10-22 14:15:40 +0200

    Update urls.py for root redirection

--- olympics_ticketing/urls.py
+++ olympics_ticketing/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reservations/', include('reservations.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),  # Ajout des URLs de connexion
-]
+    path('accounts/', include('django.contrib.auth.urls')),
 
-    
+    # Redirige la racine vers /reservations/
+    path('', lambda request: HttpResponseRedirect('/reservations/')),
+]
